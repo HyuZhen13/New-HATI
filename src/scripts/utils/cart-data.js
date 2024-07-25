1933a5a2b7fc811e3a47578614412914e4295aae
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-07-25 13:18:46 +0700

    Update cart-data.js

--- src/scripts/utils/cart-data.js
+++ src/scripts/utils/cart-data.js
@@ -1,19 +1,40 @@
-// src/scripts/utils/cart-data.js
 class CartData {
-  static async addCartItem(item) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.push(item);
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-
   static getCartItems() {
     return JSON.parse(localStorage.getItem('cart')) || [];
   }
 
+  static addCartItem(item) {
+    const cart = this.getCartItems();
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
   static removeCartItem(id) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let cart = this.getCartItems();
     cart = cart.filter(item => item.id !== id);
     localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  static updateCartItem(id, quantity) {
+    const cart = this.getCartItems();
+    const item = cart.find(i => i.id === id);
+    if (item) {
+      item.quantity = quantity;
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+  }
+
+  static setPaymentProof(url) {
+    localStorage.setItem('paymentProof', url);
+  }
+
+  static getPaymentProof() {
+    return localStorage.getItem('paymentProof');
+  }
+
+  static clearCart() {
+    localStorage.removeItem('cart');
+    localStorage.removeItem('paymentProof');
   }
 }
 
