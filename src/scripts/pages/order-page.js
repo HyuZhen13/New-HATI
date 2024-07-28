b37bba8bd8a2013c1f0b236640a09fcb5948713c
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-07-28 15:53:10 +0700

    Delete total price order-page.js

--- src/scripts/pages/order-page.js
+++ src/scripts/pages/order-page.js
@@ -27,12 +27,9 @@ const OrderPage = {
 
     const order = await OrderData.getCurrentOrder();
     if (order) {
-      // Mendapatkan total harga dari order.totalPrice
-      let totalPrice = order.totalPrice;
 
       orderDetailsContainer.innerHTML = `
         <h2>Pesanan Anda</h2>
-        <p>Total Harga: ${Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(totalPrice)}</p>
         <img src="${order.paymentProof}" alt="Bukti Pembayaran">
         <div id="order-items"></div>
       `;
@@ -96,7 +93,6 @@ const OrderPage = {
           orderElement.classList.add('order');
           orderElement.innerHTML = `
             <h2>Pesanan Selesai - ${order.id}</h2>
-            <p>Total Harga: ${Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(order.totalPrice)}</p>
             <img src="${order.paymentProof}" alt="Bukti Pembayaran">
             <div class="order-items">
               ${order.items.map(item => `
