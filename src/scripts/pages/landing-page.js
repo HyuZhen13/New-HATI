a8ba0a4b8601ea51600609a5dd67dbc14b660ef0
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-08-03 13:08:54 +0700

    Update landing-page.js

--- src/scripts/pages/landing-page.js
+++ src/scripts/pages/landing-page.js
@@ -1,52 +1,15 @@
 import { getAuth } from 'firebase/auth';
+import './landing-page.css'; // Pastikan Anda menyesuaikan path sesuai dengan struktur proyek Anda
 
 const LandingPage = {
   async render() {
     return `
-      <style>
-        body {
-          background-color: #f0f0f0; /* Ganti dengan warna background body yang sesuai */
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: space-between;
-          min-height: 100vh;
-          margin: 0;
-        }
-        .welcome-message {
-          font-size: 24px;
-          font-weight: bold;
-          margin-top: 20px;
-          text-align: center;
-        }
-        .view-news-button {
-          font-size: 16px;
-          background-color: #333; /* Warna tombol lebih gelap */
-          color: white;
-          border: none;
-          padding: 10px 20px;
-          margin-bottom: 20px;
-          cursor: pointer;
-          border-radius: 5px;
-          text-align: center;
-        }
-        .view-news-button:hover {
-          background-color: #555; /* Warna lebih gelap saat tombol di-hover */
-        }
-        .auth-container {
-          background-color: white;
-          padding: 20px;
-          border-radius: 10px;
-          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-          text-align: center;
-        }
-      </style>
       <div class="welcome-message">Selamat datang di Sistem Informasi Pertanian Kelompok Tani Sri Rahayu 3</div>
-      <article class="auth-article"> 
+      <section class="auth-section"> 
         <div class="auth-container">
           <button id="loginSystem" class="login-system-button">Login ke Sistem</button>
         </div>
-      </article>
+      </section>
       <button id="viewNews" class="view-news-button">Saya hanya ingin melihat berita</button>
     `;
   },
