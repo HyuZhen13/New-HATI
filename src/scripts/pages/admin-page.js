6202b36e882aa045a17a6f5ef3e1bd3574effebb
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-08-13 20:49:27 +0700

    Update admin-page.js

--- src/scripts/pages/admin-page.js
+++ src/scripts/pages/admin-page.js
@@ -846,20 +846,18 @@ const AdminPage = {
           hideLoadingIcon();
         }, 1000);
       });
-        
+      
+          // Fungsi untuk mencetak pesanan sebagai PDF
+          function printOrderAsPDF(order) {
+              const doc = new jsPDF();
+              doc.text(`Order ID: ${order.id}`, 10, 10);
+              doc.text(`Name: ${order.name}`, 10, 20);
+              doc.text(`Price: ${order.price}`, 10, 30);
+              doc.addImage(order.image, 'JPEG', 10, 40, 180, 160);
+              doc.save(`Order_${order.id}.pdf`);
+            }
     }
   },
-
-  // Fungsi untuk mencetak pesanan sebagai PDF
-const printOrderAsPDF(order) {
-    const doc = new jsPDF();
-    doc.text(`Order ID: ${order.id}`, 10, 10);
-    doc.text(`Name: ${order.name}`, 10, 20);
-    doc.text(`Price: ${order.price}`, 10, 30);
-    doc.addImage(order.image, 'JPEG', 10, 40, 180, 160);
-    doc.save(`Order_${order.id}.pdf`);
-  }
-
 };
 
 export default AdminPage;
