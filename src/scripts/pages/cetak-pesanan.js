6e590ee7b1c4dfcbab43202a0e25952cf312a3cc
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-08-13 22:01:14 +0700

    deletecetak-pesanan.js

--- src/scripts/pages/cetak-pesanan.js
+++ src/scripts/pages/cetak-pesanan.js
@@ -1,11 +1,1 @@
-import jsPDF from 'jspdf';
 
-// Fungsi untuk mencetak pesanan sebagai PDF
-export function printOrderAsPDF(order) {
-    const doc = new jsPDF();
-    doc.text(`Order ID: ${order.id}`, 10, 10);
-    doc.text(`Name: ${order.name}`, 10, 20);
-    doc.text(`Price: ${order.price}`, 10, 30);
-    doc.addImage(order.image, 'JPEG', 10, 40, 180, 160);
-    doc.save(`Order_${order.id}.pdf`);
-}
