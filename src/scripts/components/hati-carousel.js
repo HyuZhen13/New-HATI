bd83587c0dc8c24e70adf6c048e915ece242ba2f
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-08-21 06:31:18 +0700

    Update hati-carousel.js

--- src/scripts/components/hati-carousel.js
+++ src/scripts/components/hati-carousel.js
@@ -6,7 +6,7 @@ class hatiCarousel extends HTMLElement {
   render() {
     /* html */
     this.innerHTML = `
-        <style>
+    <style>
         /* Media query untuk layar dengan lebar kurang dari 400px */
         @media (max-width: 400px) {
           .map-iframe {
@@ -14,48 +14,49 @@ class hatiCarousel extends HTMLElement {
             height: 150px;
           }
         }
-      </style>
+    </style>
     <div id="carouselExampleCaptions" class="carousel slide">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="./Images/hati.png" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-          </div>
-        </div>  
-        <div class="carousel-item">
-           <iframe
-            class="map-iframe"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31665.70233490767!2d109.34238815685402!3d-7.378130662177316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e655ef01e1cf687%3A0xf5b8e4d02830202e!2s7%C2%B022&#39;41.3%22S%20109%C2%B020&#39;44.6%22E!5e0!3m2!1sen!2sid!4v1691210738485!5m2!1sen!2sid"
-            width="1100px"
-            height="470px"
-            style="border:0;"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
-          <div class="carousel-caption d-none d-md-block">
-          </div>
-          <div class="carousel-item active">
-            <img src="./Images/hati.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-            </div>
-          </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    `;
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./Images/hati.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+      <div class="carousel-item">
+        <iframe
+          class="map-iframe"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31665.70233490767!2d109.34238815685402!3d-7.378130662177316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e655ef01e1cf687%3A0xf5b8e4d02830202e!2s7%C2%B022&#39;41.3%22S%20109%C2%B020&#39;44.6%22E!5e0!3m2!1sen!2sid!4v1691210738485!5m2!1sen!2sid"
+          width="1100px"
+          height="470px"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./Images/hati.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+        `;
   }
 }
 
