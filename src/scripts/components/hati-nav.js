da2a0aed3a3418d5c9fa8208593806c56db32e2b
HyuZhen13 <113155209+HyuZhen13@users.noreply.github.com>
2024-08-22 06:29:24 +0700

    Add heature hati-nav.js

--- src/scripts/components/hati-nav.js
+++ src/scripts/components/hati-nav.js
@@ -9,67 +9,69 @@ class hatiNav extends HTMLElement {
     if (UserInfo.getUserInfo().uid) {
       this.innerHTML = `
         <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand mb-0 h1" href="#">
-                <img class="lazyload" crossorigin="anonymous" src="./favicon.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
-                HATI
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+          <div class="container-fluid">
+            <a class="navbar-brand mb-0 h1" href="#">
+              <img class="lazyload" crossorigin="anonymous" src="./favicon.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
+              HATI
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="#/home">Home</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#/marketplace">Marketplace</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#/cart">Keranjang</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#/order">Pesanan</a>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="featuresDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Features
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="featuresDropdown">
+                    <li><a class="dropdown-item" href="#/marketplace">Marketplace</a></li>
+                    <li><a class="dropdown-item" href="#/cart">Keranjang</a></li>
+                    <li><a class="dropdown-item" href="#/order">Pesanan</a></li>
+                    <li><a class="dropdown-item" href="#/news">Berita</a></li>
+                  </ul>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link active" href="#/about">About Us</a>
                 </li>
               </ul>
               <form class="d-flex" role="search">
-                <a href="#/profile" class="btn btn-outline-dark btn-login" type="submit" id="btn-login" style="padding:0;"><img id="profile-button" src="./Images/profile.png"></a>
+                <a href="#/profile" class="btn btn-outline-dark btn-login" type="submit" id="btn-login" style="padding:0;">
+                  <img id="profile-button" src="./Images/profile.png" alt="Profile" width="40" height="40">
+                </a>
               </form>
             </div>
           </div>
         </nav>
-        `;
-      const profileButton = document.querySelector('#profile-button');
-      profileButton.addEventListener('click', () => {
-        location.href = '#/profile';
-      });
+      `;
     } else {
       this.innerHTML = `
         <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand mb-0 h1" href="#">
-                <img src="./favicon.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
-                HATI
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+          <div class="container-fluid">
+            <a class="navbar-brand mb-0 h1" href="#">
+              <img src="./favicon.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
+              HATI
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="#/home">Home</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#/marketplace">Marketplace</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#/cart">Keranjang</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#/order">Pesanan</a>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="featuresDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Features
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="featuresDropdown">
+                    <li><a class="dropdown-item" href="#/marketplace">Marketplace</a></li>
+                    <li><a class="dropdown-item" href="#/cart">Keranjang</a></li>
+                    <li><a class="dropdown-item" href="#/order">Pesanan</a></li>
+                    <li><a class="dropdown-item" href="#/news">Berita</a></li>
+                  </ul>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link active" href="#/about">About Us</a>
@@ -81,7 +83,7 @@ class hatiNav extends HTMLElement {
             </div>
           </div>
         </nav>
-        `;
+      `;
     }
   }
 }
